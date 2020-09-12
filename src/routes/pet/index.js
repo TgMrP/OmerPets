f443f87fcd69a76790f01514e7b9e3f3f94b0b24
Ran Tayar <tgmrp.dev@gmail.com>
2020-09-12 11:13:45 +0300

    Fixing the routing /pet

--- src/routes/pet/index.js
+++ src/routes/pet/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const controller = require('./controller');
 
+router.get('/', (req, res) => {
+  res.redirect('/');
+});
 router.get('/all', controller.all);
 router.get('/create', controller.create);
 router.get('/:id', controller.get);
